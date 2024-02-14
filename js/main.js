5a0d701a454f16b3ae728bfabb7f06bbbec800ed
Shlomi-Afrimi <shlomi.afr18@gmail.com>
2024-02-14 08:39:37 +0200

    Limit size 400px

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 var ballSize = 100
 function onBallClick(){
     ballSize = ballSize + 50
+    if(ballSize > 400) ballSize = 100
     var elBall = document.querySelector('.ball')
     elBall.style.width = ballSize +'px'
     elBall.style.height = ballSize + 'px'
     elBall.innerText = ballSize
-    // elBall.style.line-height = ballSize +'px'
 }
